c61d19dda32bf828e0980b5a4836abdaabfedae3
uabdulrahman106@gmail.com <uabdulrahman106@gmail.com>
2023-10-07 19:02:54 +0100

    commit

--- script/script.js
+++
@@ -1,7 +0,0 @@
-var toBeTyped = document.getElementById("hero");
-var typed = document.getElementById("myName");
-
-var typed = new Typed(typed, {
-    stringsElement: toBeTyped,
-    typeSpeed: 100,
-});
