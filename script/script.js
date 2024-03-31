f076f0b62cb76917664b6b981f45d0d371d43e6f
abcreative1 <uabdulrahman106@gmial.com>
2024-03-31 10:52:21 +0100

    commit

--- script/script.js
+++ script/script.js
@@ -7,14 +7,49 @@ nav_button.addEventListener("click", openSideNav)
 
 close_button.addEventListener("click", closeSideNav)
 
-for (let i = 0; i < navlink.length; i++){
+for (let i = 0; i < navlink.length; i++) {
   navlink[i].addEventListener("click", closeSideNav)
 }
 
-function closeSideNav(){
-    nav.style.width = "0"
+function closeSideNav() {
+  nav.style.width = "0"
 }
 
-function openSideNav(){
-    nav.style.width = "100vw"
+function openSideNav() {
+  nav.style.width = "100vw"
 }
+
+function animateaOnScroll() {
+
+  var cards = document.querySelectorAll('.card');
+  var skillCards = document.querySelectorAll('.skill-item')
+  var serviceCards = document.querySelectorAll('.service-card')
+
+  cards.forEach(function (card) {
+
+    card.setAttribute('data-aos', 'fade-up');
+
+
+  });
+
+
+  skillCards.forEach(function (skillcard) {
+
+    skillcard.setAttribute('data-aos', 'fade-up');
+
+  });
+  
+  serviceCards.forEach(function(serviceCard){
+    serviceCard.setAttribute('data-aos', 'fade-up')
+  });
+
+}
+animateaOnScroll()
+const typed = new Typed('#typed', {
+  stringsElement: '#typed-strings',
+  typeSpeed: 40,
+  backDelay: 1000,
+  loop: true,
+  loopCount: 3,
+  cursorChar: '_'
+});
