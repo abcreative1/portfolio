1778b742423ebb5723c2a9aedd40287b5b428ca0
abcreative1 <uabdulrahman106@gmail.com>
2025-03-13 21:48:56 +0100

    style improvements

--- script/script.js
+++ script/script.js
@@ -1,23 +1,23 @@
-const nav_button = document.querySelector(".sidenav-toggle")
-const nav = document.querySelector(".offcanvas-nav")
-const close_button = document.querySelector(".btn-close")
-const navlink = document.querySelectorAll(".nav-link")
+// const nav_button = document.querySelector(".sidenav-toggle")
+// const nav = document.querySelector(".offcanvas-nav")
+// const close_button = document.querySelector(".btn-close")
+// const navlink = document.querySelectorAll(".nav-link")
 
-nav_button.addEventListener("click", openSideNav)
+// nav_button.addEventListener("click", openSideNav)
 
-close_button.addEventListener("click", closeSideNav)
+// close_button.addEventListener("click", closeSideNav)
 
-for (let i = 0; i < navlink.length; i++) {
-  navlink[i].addEventListener("click", closeSideNav)
-}
+// for (let i = 0; i < navlink.length; i++) {
+//   navlink[i].addEventListener("click", closeSideNav)
+// }
 
-function closeSideNav() {
-  nav.style.width = "0"
-}
+// function closeSideNav() {
+//   nav.style.width = "0"
+// }
 
-function openSideNav() {
-  nav.style.width = "100vw"
-}
+// function openSideNav() {
+//   nav.style.width = "100vw"
+// }
 
 function animateOnScroll() {
 
@@ -26,10 +26,8 @@ function animateOnScroll() {
   var skillCards = document.querySelectorAll('.skill-item')
   var serviceCards = document.querySelectorAll('.service-card')
 
-
-
-    about.setAttribute('data-aos', 'fade-up');
-    about.setAttribute('data-aos-once', 'true');
+  about.setAttribute('data-aos', 'fade-up');
+  about.setAttribute('data-aos-once', 'true');
 
   cards.forEach(function (card) {
 
